ee436e0684e7426d3f4868c839380cdfbadf8ebc
Kenji YAMADA <kenzauros@users.noreply.github.com>
2021-08-28 10:50:23 +0900

    Fix bio component to tsx and add github icon

---
+++ src/components/bio.tsx
@@ -0,0 +1,53 @@
+import * as React from "react"
+import { GatsbyImage, getImage, IGatsbyImageData, StaticImage } from "gatsby-plugin-image"
+
+type Props = {
+  bio: string
+  github: string
+  name: string
+  avatarImage: IGatsbyImageData
+};
+
+const Bio = (props: Props) => {
+  const bio = props.bio || null;
+  const github = props.github;
+  const name = props.name || '名無しの権兵衛';
+  const githubUrl = `https://github.com/${github}`;
+  const avatarImage = getImage(props.avatarImage)
+
+  return (
+    <div className="bio">
+      { avatarImage
+        ? <GatsbyImage
+            image={avatarImage}
+            className="bio-avatar"
+            alt={github}
+            />
+        : <StaticImage
+            src="https://www.gravatar.com/avatar/?d=mp&s=50"
+            className="bio-avatar"
+            width={50}
+            height={50}
+            layout="fixed"
+            alt={`${github} - GitHub`}
+            />}
+      <div>
+        <p>
+          <strong>{name}</strong>
+          <a href={githubUrl} target="_blank" rel="noreferrer" title={github}>
+            <StaticImage
+              src="../images/github-icon-light.png"
+              width={20}
+              height={20}
+              alt={`${github} - GitHub`}
+              />
+            @{github}
+          </a>
+        </p>
+        <p>{bio}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Bio
