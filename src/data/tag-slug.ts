75230d33d20cea0841eb6cfb5286f954ee83ca97
Kenji YAMADA <kenzauros@users.noreply.github.com>
2021-09-03 23:51:46 +0900

    Add tag slug mappings

--- src/data/tag-slug.ts
+++ src/data/tag-slug.ts
@@ -5,6 +5,12 @@ interface TagSlugs {
 const tagSlugs: TagSlugs = {
   "C#": "c-sharp",
   "ISMS": "isms",
+  "ライフハック": "lifehacks",
+  ".NET": "dotnet",
+  ".NET Core": "dotnet-core",
+  ".NET Framework": "dotnet-framework",
+  "4k": "4k-resolution",
+  "7z": "7-zip",
 }
 
 export default tagSlugs
