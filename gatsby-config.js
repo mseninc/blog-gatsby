c0159e3ddfc7324505601b9217a63444494d8065
Kenji YAMADA <kenzauros@users.noreply.github.com>
2021-09-03 10:48:27 +0900

    Fix RSS feed post count limit

--- gatsby-config.js
+++ gatsby-config.js
@@ -103,6 +103,7 @@ module.exports = {
               {
                 allMarkdownRemark(
                   sort: { order: DESC, fields: [frontmatter___date] },
+                  limit: 100
                 ) {
                   nodes {
                     excerpt
