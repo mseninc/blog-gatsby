5614c213614c3fab695a71f25a71c7bc0986b7a6
Kenji YAMADA <kenzauros@users.noreply.github.com>
2021-09-04 11:20:22 +0900

    Revert "Recover author.yaml"
    This reverts commit ff8d08c67aa7148dc094762978f4eb4f8dcf2cb3.

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,13 +39,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `data`,
-        path: `${__dirname}/src/data`,
-      },
-    },
-    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
