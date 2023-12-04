06cc4b86fcfacfd8c0885688952c655545c24076
Junwoo LEE <Lee-juNu@users.noreply.github.com>
2023-12-04 17:59:16 +0900

    数式入力のための Katex Plugin を追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,6 +60,13 @@ const plugins = [
     options: {
       plugins: [
         {
+          resolve: `gatsby-remark-katex`,
+          options: {
+            // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
+            strict: `ignore`,
+          },
+        },
+        {
           resolve: `gatsby-remark-images`,
           options: {
             showCaptions: true,
