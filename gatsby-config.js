ec183056c5f13b3122fb7e3f8fcee0f840f00402
Kenji YAMADA <kenzauros@users.noreply.github.com>
2024-03-16 16:40:51 +0900

    analytics を gtag に変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -206,9 +206,14 @@ const plugins = [
 
 if (process.env.GA_TRACKING_ID) {
   plugins.push({
-    resolve: `gatsby-plugin-google-analytics`,
+    resolve: `gatsby-plugin-google-gtag`,
     options: {
-      trackingId: process.env.GA_TRACKING_ID,
+      trackingIds: [
+        process.env.GA_TRACKING_ID, // Google Analytics / GA
+      ],
+      pluginConfig: {
+        head: true,
+      },
     },
   })
 }
